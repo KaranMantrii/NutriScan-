9563758538d1efc5ca668094dd06147130ffe02a
Yachamkaushik <yachamkaushik@gmail.com>
2026-05-23 18:10:26 +0530

    Add fitness tracking files and updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Index from './pages/landing/Index.jsx';
+import Fitness from './pages/fitness.jsx';
+
 
 import './styles.css';
 function App() {
   return (
-        <Index />
+      <Index/>
   )
 }
 export default App;
