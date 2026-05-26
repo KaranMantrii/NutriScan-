ed1696eee17bd506f12fae23ed741f7af09f54d4
KaranMantrii <karanmantri40@gmail.com>
2026-05-26 16:57:12 +0530

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Index from './pages/landing/Index.jsx';
 import Fitness from './pages/fitness.jsx';
-import Result from './pages/results.jsx';
+import Result from './pages/Results.jsx';
 
 import './styles.css';
 function App() {
